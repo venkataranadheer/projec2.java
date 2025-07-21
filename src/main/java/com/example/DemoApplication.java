2acabebab6a37b86fed6a7abe91580e9bdbc7560
venkataranadheer <venkataranadheer123@gmail.com>
2025-07-21 20:52:07 +0530

    Create DemoApplication.java

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello, Spring Boot is running!";
+    }
+}
